4485051b1b4243bbe574bbe506c1062c848c367f
Peter Reutemann <fracpete@gmail.com>
2024-05-15 09:59:06 +1200

    added a page on JNI and wrapper approaches

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-weka-wrapper3'
-copyright = u'2014-2022, Peter "fracpete" Reutemann'
+copyright = u'2014-2024, Peter "fracpete" Reutemann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
